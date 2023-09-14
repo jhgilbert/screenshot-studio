dd4884d5b36929803d20ccf1244ab950b57712a8
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-09-13 22:05:16 -0500

    Log to console when the document is clicked

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,5 +1,11 @@
 console.log("content loaded");
 
+document.addEventListener("click", function () {
+  chrome.runtime.sendMessage({}, function () {
+    console.log("document clicked");
+  });
+});
+
 /**
  * @description
  * Chrome extensions don't support modules in content scripts.
--- src/pages/sidePanel/SidePanel.tsx
+++ src/pages/sidePanel/SidePanel.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "@pages/panel/Panel.css";
 
+const handleClick = () => {
+  console.log("Clicked!");
+};
+
 const SidePanel: React.FC = () => {
   return (
-    <div className="container">
-      <h1 className="text-lime-400">Side Panel</h1>
+    <div>
+      <div>This is where image editing stuff will go.</div>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 };
