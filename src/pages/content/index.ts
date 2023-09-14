ca7f4bccb1d38627bc6cf6a473658c1156a4abaf
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-09-13 23:01:47 -0500

    Prevent default on click

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,14 +1,22 @@
 console.log("content loaded");
 
 let selectedNode: HTMLElement | null = null;
-const SELECTED_NODE_CLASSNAME = "selected-page-node";
+
+function selectNode(node: HTMLElement) {
+  deselectNode(selectedNode);
+  selectedNode = node;
+  node.style.outline = "2px solid hotpink";
+}
+
+function deselectNode(node: HTMLElement | null) {
+  if (!node) return;
+  node.style.outline = "";
+}
 
 document.addEventListener("click", function (e: PointerEvent) {
+  e.preventDefault();
   const target = e.target as HTMLElement;
-  selectedNode?.classList.remove(SELECTED_NODE_CLASSNAME);
-  console.log("Selected node classlist is ", selectedNode?.classList);
-  selectedNode = target;
-  target.classList.add(SELECTED_NODE_CLASSNAME);
+  selectNode(target);
   chrome.runtime.sendMessage("document-clicked", function (response) {
     console.log("event is ", e);
     console.log("selectedNode is ", selectedNode);
