51a1a280db4ce5d9d1235d7ae8faf48ba7645e2d
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-09-13 22:22:18 -0500

    Only select one node at a time

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,10 +1,17 @@
 console.log("content loaded");
 
+let selectedNode: HTMLElement | null = null;
+const SELECTED_NODE_CLASSNAME = "selected-page-node";
+
 document.addEventListener("click", function (e: PointerEvent) {
   const target = e.target as HTMLElement;
-  target.classList.add("selected-page-node");
+  selectedNode?.classList.remove(SELECTED_NODE_CLASSNAME);
+  console.log("Selected node classlist is ", selectedNode?.classList);
+  selectedNode = target;
+  target.classList.add(SELECTED_NODE_CLASSNAME);
   chrome.runtime.sendMessage("document-clicked", function (response) {
     console.log("event is ", e);
+    console.log("selectedNode is ", selectedNode);
     console.log("document clicked");
     console.log("response is ", response);
   });
