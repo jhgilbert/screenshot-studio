92bc1ab8c4c6b03835aad12e65288881d509030b
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-09-13 23:53:48 -0500

    Fix messaging between extension components

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -8,13 +8,27 @@ function selectNode(node: HTMLElement) {
   node.style.outline = "2px solid hotpink";
 }
 
+chrome.runtime.onMessage.addListener(function (
+  message: { type: string },
+  sender,
+  sendResponse
+) {
+  if (message.type === "select-parent") {
+    selectNode(selectedNode?.parentElement!);
+  } else if (message.type === "blur-selected" && selectedNode) {
+    console.log("blurSelected message received");
+    selectedNode.style.filter = "blur(5px)";
+  }
+  sendResponse("ack");
+});
+
 function deselectNode(node: HTMLElement | null) {
   if (!node) return;
   node.style.outline = "";
 }
 
 document.addEventListener("click", function (e: PointerEvent) {
-  e.preventDefault();
+  // e.preventDefault();
   const target = e.target as HTMLElement;
   selectNode(target);
   chrome.runtime.sendMessage("document-clicked", function (response) {
--- src/pages/sidePanel/SidePanel.tsx
+++ src/pages/sidePanel/SidePanel.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import "@pages/panel/Panel.css";
 
-const handleClick = () => {
-  console.log("Clicked!");
+const blurSelected = async () => {
+  console.log("Sending blurSelected message");
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  await chrome.tabs.sendMessage(tab.id, { type: "blur-selected" });
+};
+
+const selectParent = async () => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  await chrome.tabs.sendMessage(tab.id, { type: "select-parent" });
 };
 
 const SidePanel: React.FC = () => {
   return (
     <div>
-      <div>This is where image editing stuff will go.</div>
-      <button onClick={handleClick}>Click me!</button>
+      <p>4</p>
+      <button onClick={blurSelected}>Blur selected</button>
+      <button onClick={selectParent}>Select parent</button>
+      <hr />
+      <div>"Edit mode" toggle.</div>
+      <div>Page menu</div>
+      <ul>
+        <li>Obscure PII</li>
+      </ul>
+      <div>Select menu:</div>
+      <ul>
+        <li>Select none</li>
+        <li>Select parent</li>
+      </ul>
+      <div>Selected item menu:</div>
+      <ul>
+        <li>Blur button</li>
+        <li>Reset</li>
+      </ul>
     </div>
   );
 };
