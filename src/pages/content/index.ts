cdc15a2a2c4952c398f032460fd77a9fa2c8adda
Jennifer Gilbert <j.h.gilbert@gmail.com>
2023-09-23 17:28:37 -0500

    Refactor message responses

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -9,7 +9,6 @@ import { blurMore, blurLess, getCurrentBlurLevel } from "./nodeOperations/blur";
 import { addLabel, removeLabel, hasLabel } from "./nodeOperations/label";
 import { addShowcase, removeShowcase } from "./nodeOperations/showcase";
 
-let selectedNode: HTMLElement | null = null;
 let extensionIsActive: boolean = false;
 
 async function syncWithSidePanel() {
@@ -20,21 +19,30 @@ async function syncWithSidePanel() {
       payload: extensionIsActive,
     })
     .catch((e) => console.log(e));
-  broadcastSelectionData();
+  const selectedNode = getSelectedNode(document);
+  broadcastSelectionData(selectedNode);
+}
+
+function getSelectedNode(document: Document): HTMLElement | null {
+  const selectedNodes = document.getElementsByClassName(SELECTED_NODE_CLASS);
+  if (selectedNodes.length === 0) return null;
+  if (selectedNodes.length > 1) {
+    console.error("More than one selected node found");
+  }
+  return selectedNodes[0] as HTMLElement;
 }
 
 syncWithSidePanel();
 
 function selectNode(node: HTMLElement) {
   selectNone();
-  selectedNode = node;
-  selectedNode.classList.add(SELECTED_NODE_CLASS);
-  if (selectedNode.classList.contains(LABEL_TAB_CLASS)) {
+  node.classList.add(SELECTED_NODE_CLASS);
+  if (node.classList.contains(LABEL_TAB_CLASS)) {
     document.body.contentEditable = "false";
   } else {
     document.body.contentEditable = "true";
   }
-  broadcastSelectionData();
+  broadcastSelectionData(node);
 }
 
 const buildSelectedNodeAttrs = (
@@ -50,7 +58,7 @@ const buildSelectedNodeAttrs = (
   return attrs;
 };
 
-async function broadcastSelectionData() {
+async function broadcastSelectionData(selectedNode: HTMLElement | null) {
   if (!selectedNode) {
     await chrome.runtime
       .sendMessage({
@@ -78,6 +86,7 @@ function deselectNode(node: HTMLElement) {
 }
 
 function selectNone() {
+  const selectedNode = getSelectedNode(document);
   if (!selectedNode) return;
   deselectNode(selectedNode);
 }
@@ -93,27 +102,10 @@ chrome.runtime.onMessage.addListener(async function (
     console.log("Message received, but page is hidden:", message);
     return;
   }
-  if (message.type === "select-parent") {
-    selectNode(selectedNode?.parentElement!);
-  } else if (message.type === "blur-selected-more" && selectedNode) {
-    blurMore(selectedNode);
-  } else if (message.type === "blur-selected-less" && selectedNode) {
-    blurLess(selectedNode);
-  } else if (message.type === "delete-selected" && selectedNode) {
-    selectedNode.style.display = "none";
-    deselectNode(selectedNode);
-  } else if (message.type === "hide-selected" && selectedNode) {
-    selectedNode.style.visibility = "hidden";
-  } else if (message.type === "show-selected" && selectedNode) {
-    selectedNode.style.visibility = "visible";
-  } else if (message.type === "showcase-selected" && selectedNode) {
-    addShowcase(selectedNode);
-  } else if (message.type === "unshowcase-selected" && selectedNode) {
-    removeShowcase(selectedNode);
-  } else if (message.type === "obscure-pii") {
+
+  // handle page operations
+  if (message.type === "obscure-pii") {
     obscurePii(document);
-  } else if (message.type === "select-none" && selectedNode) {
-    deselectNode(selectedNode);
   } else if (message.type === "set-extension-is-active") {
     extensionIsActive = message.payload;
     if (!extensionIsActive) {
@@ -123,14 +115,47 @@ chrome.runtime.onMessage.addListener(async function (
       document.body.contentEditable = "true";
       addPageEventListeners();
     }
-  } else if (message.type === "label-selected") {
-    if (!selectedNode) return;
-    const labelNode = addLabel(selectedNode);
-    selectNode(labelNode);
-  } else if (message.type === "unlabel-selected") {
-    if (!selectedNode) return;
-    removeLabel(selectedNode);
   }
+
+  // handle node operations
+  const selectedNode = getSelectedNode(document);
+  if (!selectedNode) return;
+
+  switch (message.type) {
+    case "blur-selected-more":
+      blurMore(selectedNode);
+      break;
+    case "blur-selected-less":
+      blurLess(selectedNode);
+      break;
+    case "delete-selected":
+      selectedNode.style.display = "none";
+      deselectNode(selectedNode);
+      break;
+    case "hide-selected":
+      selectedNode.style.visibility = "hidden";
+      break;
+    case "show-selected":
+      selectedNode.style.visibility = "visible";
+      break;
+    case "showcase-selected":
+      addShowcase(selectedNode);
+      break;
+    case "unshowcase-selected":
+      removeShowcase(selectedNode);
+      break;
+    case "select-none":
+      deselectNode(selectedNode);
+      break;
+    case "label-selected":
+      const labelNode = addLabel(selectedNode);
+      selectNode(labelNode);
+      break;
+    case "unlabel-selected":
+      removeLabel(selectedNode);
+      break;
+  }
+
   // await broadcastSelectionData();
   // sendResponse(buildSelectedNodeAttrs());
 });
